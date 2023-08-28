2478cc0d354f9378f6ccf04f3a72dad3e5b4e400
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-08-28 11:23:24 +0700

    initial code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, {Express, Request, Response} from 'express'
 const app:Express = express()
 
 app.get("/", (req:Request,res:Response)=>{
-    res.send('Hello TypeScript')
+    res.send('Hello TypeSasfasfcriasdpt')
 })
 
 app.listen(3000, ()=>{
